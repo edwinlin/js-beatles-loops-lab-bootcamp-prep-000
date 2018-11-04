992661f6fdcc762eab4fd888cca8820c83602888
Edwin Lin <edwinlin@gmail.com>
2018-11-04 04:20:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // add solution here
-function theBeatlesPlay(){
-  
+function theBeatlesPlay(musicans, instruments){
+  var emptyArray = {}
+  for(var i=0; i< musicians.length; i++){
+      musicians[i].toString + " plays " + instruments[i]
+    }
+  }
 }
