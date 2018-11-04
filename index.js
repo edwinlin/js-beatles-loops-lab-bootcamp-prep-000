f6056d6a8d3df9dd6907c976b7c46b63c82b1665
Edwin Lin <edwinlin@gmail.com>
2018-11-04 04:28:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 // add solution here
 function theBeatlesPlay(musicans, instruments){
-  var emptyArray = {}
+  var emptyArray = []
   for(var i=0; i< musicians.length; i++){
-      musicians[i].toString + " plays " + instruments[i]
-    }
+      emptyArray.push(musicians[i].toString() + " plays " + instruments[i].toString())
   }
+  return emptyArray
+}
+
+function johnLennonFacts(facts){
+  var array = []
+  for(var i = 0; i< facts.length; i++){
+    array.push(facts[i].toString() + "!!!")
+  }
+  return array
+}
+
+function iLoveTheBeatles(number){
+  var strings = []
+  do{
+    empty.push("I love the Beatles!")
+  } while(number<15 && number++)
+  return strings
 }
